b253a5059a4c6914382130ee86d30568cbf1f1c5
Hardeep Sidhu <hss70@bath.ac.uk>
2025-11-05 13:32:22 +0000

    fix: update Classifier Ready message formatting and sessionId filter in CloudWatch query

--- powershellTestScripts/InfraChecks.ps1
+++ powershellTestScripts/InfraChecks.ps1
@@ -1,2 +1,3 @@
 sam build --template-file ..\infra\trainingPipelineTemplate.yaml --region eu-west-2
 sam validate --template-file ..\infra\trainingPipelineTemplate.yaml --region eu-west-2 --lint
+aws stepfunctions validate-state-machine-definition --region eu-west-2 --definition ..\infra\EEGProcessingStateMachine.asl.json
